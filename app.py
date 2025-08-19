a68379bf12dfeb00a372b1510d93ff7b510366d6
sanjeevak2012 <your-email@example.com>
2025-08-19 10:37:53 +0530

    Updated Flask app for Kubernetes

--- app.py
+++ app.py
@@ -8,3 +8,13 @@ def hello():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello from MyWebApp running on Kubernetes! [Updated]"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
